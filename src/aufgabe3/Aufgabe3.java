5b1884132e0ee4055d6926c957bcffa2f6a62580
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-08 14:49:14 +0100

    loesung aufgabe1

---
+++ src/aufgabe3/Aufgabe3.java
@@ -0,0 +1,15 @@
+package aufgabe3;
+
+public class Aufgabe3 {
+
+    public static void main(String[] args) {
+        for(int i=0; i < 10; i++) {
+            if(i == 3){
+                break;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+    }
+
