be322b8edb39d91bdc98802f4978e55314b6edd3
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-09 15:02:11 +0100

    loesung Aufgabe9

---
+++ src/aufgabe9/BmiClassification.java
@@ -0,0 +1,12 @@
+package aufgabe9;
+
+/**
+ * Class definition, declare as separate file
+ * @param
+ * @param
+ * @return
+ */
+public enum BmiClassification {
+        UNDERWEIGHT, NORMAL, OVERWEIGHT, OBESE
+    }
+
