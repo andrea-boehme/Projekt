2c709d9689b8be0ec474c1db02bfe530597340dd
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-09 22:34:21 +0100

    loesung Aufgabe3

--- src/aufgabe3/Aufgabe3.java
+++ src/aufgabe3/Aufgabe3.java
@@ -3,6 +3,7 @@ package aufgabe3;
 public class Aufgabe3 {
 
     public static void main(String[] args) {
+
         for(int i=0; i < 10; i++) {
             if(i == 3){
                 break;
@@ -11,5 +12,5 @@ public class Aufgabe3 {
             }
         }
     }
-    }
+}
 
