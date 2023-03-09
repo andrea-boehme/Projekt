0a8842bdb5cb0d08c40db3aee8b7b4b23deac5cf
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-09 16:13:13 +0100

    loesung Aufgabe11

---
+++ src/aufgabe11/GuessingGame.java
@@ -0,0 +1,32 @@
+package aufgabe11;
+
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GuessingGame {
+
+    static int randomNumber = ThreadLocalRandom.current().nextInt(0, 100 +1);
+
+    public static void main(String args[]) {
+        newGuess();
+    }
+
+    public static void newGuess() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please guess number: ");
+        int number = scanner.nextInt();
+        guess(number);
+    }
+
+    public static void guess( int number) {
+        if(number == randomNumber) {
+            System.out.println("Richtig geraten");
+        } else if(number < randomNumber) {
+            System.out.println("Bitte höhere Zahl raten");
+        } else if(number > randomNumber) {
+            System.out.println("Bitte niedrigere Zahl raten");
+        }
+        newGuess();
+    }
+}
