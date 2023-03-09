e7ecb4553265e92402e3cc5bc5c88cd74b6a1744
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-09 22:34:36 +0100

    loesung Aufgabe6

--- src/aufgabe6/Aufgabe6.java
+++ src/aufgabe6/Aufgabe6.java
@@ -2,18 +2,21 @@ package aufgabe6;
 
 public class Aufgabe6 {
 
+    //    public static void main(String[] args){}
+
         /**
          * A number divided by two, which doesn't result in a rest of zero
-         * is divisable by two.2
+         * is divisible by two
          * @param number dividend
-         * @return boolean is number diivisable by two
+         * @return boolean is number divisible by two
          */
 
-        public static boolean isDivisibleByTwo(int number) {
-            if (number % 2 == 0){
+        public static boolean isDivisibleByTwo ( int number){
+            if (number % 2 == 0) {
                 return true;
             } else {
                 return false;
             }
         }
+
 }
