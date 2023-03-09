0b0e0efa134e51d1bf262e3246479af3bf2ec139
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-09 08:15:27 +0100

    loesung Aufgabe5

---
+++ src/aufgabe5/Aufgabe5.java
@@ -0,0 +1,13 @@
+package aufgabe5;
+
+public class Aufgabe5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 3) {
+                return;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
