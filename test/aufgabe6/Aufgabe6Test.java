830788bdcfdad9c36db1656d66807cc42be306ea
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-18 22:43:49 +0100

    loesung Aufgabe6

--- test/aufgabe6/Aufgabe6Test.java
+++ test/aufgabe6/Aufgabe6Test.java
@@ -8,11 +8,11 @@ class Aufgabe6Test {
 
     @Test
     public void DivisibleByTwo() {
-        assertTrue(Aufgabe6.isDivisibleByTwo(2));
-        assertTrue(Aufgabe6.isDivisibleByTwo(10));
+        assertTrue(DivisibleByTwo.isDivisibleByTwo(2));
+        assertTrue(DivisibleByTwo.isDivisibleByTwo(10));
     }
     @Test
     public void NotDivisibleByTwo() {
-        assertFalse(Aufgabe6.isDivisibleByTwo(3));
+        assertFalse(DivisibleByTwo.isDivisibleByTwo(3));
     }
 }
