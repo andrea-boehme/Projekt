123950f2e6a8bcb7324e72f2758629dd7da1f2d2
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-18 22:46:13 +0100

    loesung Aufgabe6

--- test/aufgabe6/DivisibleByTwoTest.java
+++ test/aufgabe6/DivisibleByTwoTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class Aufgabe6Test {
+class DivisibleByTwoTest {
 
     @Test
     public void DivisibleByTwo() {
